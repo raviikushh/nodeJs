6c79d61abee18b38e5107ddcab640e671f42ef8c
Ravi Kumar kushwaha <ravikushwaha2031@gmail.com>
2025-08-24 23:44:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const fs = require('fs')
+
+// fs.writeFile('hello.txt', 'Hello world!',(err)=>{
+//     if(err) console.log(err, "Error in writing file")
+//     else console.log('File written successfully');
+// })
+
+
+// fs.appendFile('hello.txt', 'How are you?',(err)=>{
+//     if(err) console.log(err, "Error in writing file")
+//     else console.log('File appended successfully');
+// })
+
+// fs.rename('hello.txt', 'greetings.txt', (err) => {
+//     if (err) console.log(err, "Error in renaming file");
+//     else console.log('File renamed successfully');
+// });
+
+// fs.copyFile('greetings.txt','./copy/copy.txt', (err)=>{
+//     if (err) {
+//         console.log(err, "Error in copying file");
+//     } else {
+//         console.log('File copied successfully');
+//     }
+// })
+
+// fs.unlink('greetings.txt', (err)=>{
+//     if (err) {
+//         console.log(err, "Error in deleting file");
+//     } else {
+//         console.log('File deleted successfully');
+//     }
+// });
+
+// fs.rmdir("./copy", {recursive:true}, (err)=>{
+//     if (err) {
+//         console.log(err, "Error in deleting directory");
+//     } else {
+//         console.log('Directory deleted successfully');
+//     }
+// });
+
+fs.rm("./copy", {recursive:true}, (err)=>{
+    if (err) {
+        console.log(err, "Error in deleting directory");
+    } else {
+        console.log('Directory deleted successfully');
+    }
+});
